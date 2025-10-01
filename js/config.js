f32a4472ea30b94ce9935b3bb1cd6293f1ed1da4
flowereDnap <nikitashapko5@gmail.com>
2025-10-01 05:27:19 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// ==================== CONFIG ====================
+const CONFIG = {
+    defaultLanguage: 'ru',
+    hapticFeedback: true,
+    debugMode: false,
+    rewardAmount: 0.05,
+    videoDuration: 30, // seconds
+    videoRequiredWatchPercentage: 95 // Must watch 95% to get reward
+};
